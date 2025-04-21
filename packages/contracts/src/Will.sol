fcc3ddc44806f31ee45e77b716c70ea8eebef383
parseb <petra306@protonmail.com>
2025-04-21 21:19:55 +0300

    refactor minting logic to use currentPrice and simplify mintCost calculation

--- packages/contracts/src/Will.sol
+++ packages/contracts/src/Will.sol
@@ -80,7 +80,7 @@ contract Will is SuperchainERC20 {
         _updatePriceIfNewBlock();
         if (msg.value < lastPrice) revert ValueMismatch();
 
-        howMuchMinted = (msg.value * 1 ether) / lastPrice;
+        howMuchMinted = msg.value / currentPrice();
         _mint(msg.sender, howMuchMinted);
         
         emit WillMinted(msg.sender, howMuchMinted, msg.value);
@@ -89,10 +89,10 @@ contract Will is SuperchainERC20 {
     function mint(uint256 howMany_) public payable {
         _updatePriceIfNewBlock();
 
-        uint256 required = mintCost(howMany_);
+        uint256 required = howMany_ * currentPrice();
         if (msg.value < required) revert InsufficientValue({required: required, provided: msg.value});
         
-        _mint(msg.sender, howMany_);
+        _mint(msg.sender, howMany_ * 1e18);
         
         emit WillMinted(msg.sender, howMany_, msg.value);
     }
@@ -170,9 +170,13 @@ contract Will is SuperchainERC20 {
         entered = false;
     }
 
+
+    //// @note Returns the cost of minting a given amount of tokens
+    /// @param amt_ The amount of tokens to mint in full 1e18 units
+    /// @return The cost in wei to mint the specified amount of tokens
     function mintCost(uint256 amt_) public view virtual returns (uint256) {
         uint256 price = currentPrice();
-        return (amt_ * price) / 1 ether;
+        return (amt_ * price);
     }
 
     function burnReturns(uint256 amt_) public view virtual returns (uint256 rv) {
